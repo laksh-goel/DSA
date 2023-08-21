b5ec874dba6ada83485d41289385fbf443f290d2
laksh-goel <116437679+laksh-goel@users.noreply.github.com>
2023-08-21 21:46:16 +0530

    Program1.cpp
    #WAP to reverse an array or string using c++

---
+++ ARRAY/Program1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+         int n=str.length();
+        int start=0,end=n-1;
+        while(start<end){
+            char a =str[end];
+                str[end]=str[start];
+               str[start]=a;
+               start++;
+               end--;
+                
+            }
+            return str;
+    }
+};
+}
